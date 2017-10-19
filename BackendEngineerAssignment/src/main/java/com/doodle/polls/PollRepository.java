2e8d8f4fdb6953bb227fb0b491021fbb90a8f241
Milan Bojovic <milan.bojovic@braintribe.com>
2017-10-20 00:29:30 +0200

    Added rest endpoint for filtering on creation date

--- BackendEngineerAssignment/src/main/java/com/doodle/polls/PollRepository.java
+++ BackendEngineerAssignment/src/main/java/com/doodle/polls/PollRepository.java
@@ -2,7 +2,6 @@ package com.doodle.polls;
 
 import com.doodle.polls.beans.Poll;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -12,10 +11,8 @@ import java.util.List;
 public interface PollRepository extends MongoRepository<Poll, String> {
 
     public Poll findByAdminKey(@Param("adminKey") String adminKey);
-    public List<Poll> findByState(@Param("state") String state);
+    public List<Poll> findByInitiatedAfter(@Param("time") long time);
 
-    @Query(value = "{}", fields = "{'_id':1}")
-    public List<Poll> findAll();
-
-
+    /*@Query(value = "{}", fields = "{'_id':1}")
+    public List<Poll> findAll();*/
 }
