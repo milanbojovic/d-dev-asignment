d9d01fc2ef7e0f02532547177f1d4a1783d50fa8
Milan Bojovic <milan.bojovic@braintribe.com>
2017-10-21 18:52:31 +0200

    Added logic for textSearch

--- BackendEngineerAssignment/src/main/java/com/doodle/polls/Application.java
+++ BackendEngineerAssignment/src/main/java/com/doodle/polls/Application.java
@@ -1,32 +1,20 @@
 package com.doodle.polls;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.doodle.polls.beans.Poll;
 
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
-
-    @Autowired
-    private PollRepository repository;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @Override
-    public void run(String... args) throws Exception {
-        // fetch all Polls from database
-        System.out.println("Polls found with findAll():");
-        System.out.println("-------------------------------");
+    public void run(String... strings) throws Exception {
 
-        for (Poll poll : repository.findAll()) {
-            System.out.println(poll);
-        }
-        System.out.println();
     }
 
 }
--- BackendEngineerAssignment/src/main/java/com/doodle/polls/PollRepository.java
+++ BackendEngineerAssignment/src/main/java/com/doodle/polls/PollRepository.java
@@ -2,6 +2,7 @@ package com.doodle.polls;
 
 import com.doodle.polls.beans.Poll;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -10,9 +11,9 @@ import java.util.List;
 @RepositoryRestResource(collectionResourceRel = "polls", path = "polls")
 public interface PollRepository extends MongoRepository<Poll, String> {
 
-    public Poll findByAdminKey(@Param("adminKey") String adminKey);
-    public List<Poll> findByInitiatedAfter(@Param("time") long time);
+    List<Poll> findByInitiatedAfter(@Param("time") long time);
 
-    /*@Query(value = "{}", fields = "{'_id':1}")
-    public List<Poll> findAll();*/
+    @Query("{ $text : { $search: ?0 } }")
+    List<Poll> findByText(@Param("text") String text);
+
 }
